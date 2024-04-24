25cb685fe36778e24bd9124766b60a3cc6796840
vusal-iom <vusal@iomete.com>
2024-04-24 17:33:50 +0200

    Update terraform main configuration file
    The main terraform configuration file (main.tf) has been updated to reflect changes in documentation comments and upgrade Iomete-data-plane module version. Also, removed unnecessary prerequisites section with the relevant information merged into the appropriate comments.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,23 @@
-# Pre-requisites:
-# 1. Create a GCP project. See: https://cloud.google.com/resource-manager/docs/creating-managing-projects
-# 2. Create a Cloud Storage bucket in the project (pay attention to the location of the bucket, it should be the same as the location of the cluster)
+# Example deployment: https://github.com/iomete/terraform-gcp-iomete-data-plane/blob/main/examples/example1/main.tf
 
 module "iomete-data-plane" {
   source  = "iomete/iomete-data-plane/gcp"
-  version = "1.0.0"
+  version = "1.1.0"
 
-  # A unique cluster name for IOMETE. It should be unique within GCP project and compatible with GCP naming conventions (See: https://cloud.google.com/compute/docs/naming-resources)
+  # A user provided cluster name for IOMETE. It should be unique within GCP project and compatible with GCP naming conventions (See: https://cloud.google.com/compute/docs/naming-resources)
   cluster_name = "<cluster-name>"
 
   # Google Cloud project ID. This is a unique identifier for your project and can be found in the Google Cloud Console. Recommended to create a new project for IOMETE.
+  # To create a GCP project. See: https://cloud.google.com/resource-manager/docs/creating-managing-projects
   project_id = "<your-gcp-project-id>"
 
   # The region where the cluster and Cloud storage will be hosted
   location = "<gcp-region, e.g. us-central1>"
 
-  # The zone where the cluster will be hosted?
+  # The zone where the cluster will be hosted
   zone = "<gcp-zone, e.g. us-central1-a>"
 
-  # Create a new Bucket in your project and provide the name here
+  # Create a Cloud Storage bucket inside the GCP the project (pay attention to the location of the bucket, it should be the same as the location of the cluster) and provide the name here
   lakehouse_storage_bucket_name = "<your-bucket-name>"
 }
 
